c5025a93ffef14fea91451222765e9a1493267b5
Omar Adel <oadel1814@gmail.com>
2026-05-15 02:09:38 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -9,9 +9,13 @@ var _ = fmt.Print
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
-	fmt.Print("$ ")
-	var cmd string
-	// read a line of input and store it in the address of the "cmd" variable
-	fmt.Scanln(&cmd)
-	fmt.Printf("%s: command not found", cmd)
+	for {
+		fmt.Print("$ ")
+		var cmd string
+
+		// blocking the code until the user inputs a line of text and presses enter
+		// read a line of input and store it in the address of the "cmd" variable
+		fmt.Scanln(&cmd)
+		fmt.Printf("%s: command not found\n", cmd)
+	}
 }
