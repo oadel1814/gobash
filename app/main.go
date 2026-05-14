667582fc576be924d90fa43fff20f379a4368899
Omar Adel <oadel1814@gmail.com>
2026-05-15 02:39:30 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -18,19 +18,23 @@ func prompt() string {
 	return strings.TrimSpace(cmd)
 }
 
+func tokenize(cmd *string) []string {
+	return strings.Fields(*cmd)
+}
+
 func main() {
 	for {
 		cmd := prompt()
-		words := strings.Fields(cmd)
-		if strings.ToLower(words[0]) == "exit" {
+		tokens := tokenize(&cmd)
+		if strings.ToLower(tokens[0]) == "exit" {
 			break
-		} else if strings.ToLower(words[0]) == "echo" {
-			fmt.Println(strings.Join(words[1:], " "))
-		} else if strings.ToLower(words[0]) == "type" {
-			if slices.Contains([]string{"echo", "exit", "type"}, strings.ToLower(words[1])) == true {
-				fmt.Printf("%s is a shell builtin\n", strings.ToLower(words[1]))
+		} else if strings.ToLower(tokens[0]) == "echo" {
+			fmt.Println(strings.Join(tokens[1:], " "))
+		} else if strings.ToLower(tokens[0]) == "type" {
+			if slices.Contains([]string{"echo", "exit", "type"}, strings.ToLower(tokens[1])) == true {
+				fmt.Printf("%s is a shell builtin\n", strings.ToLower(tokens[1]))
 			} else {
-				fmt.Printf("%s: not found\n", strings.ToLower(words[1]))
+				fmt.Printf("%s: not found\n", strings.ToLower(tokens[1]))
 			}
 		} else {
 			fmt.Printf("%s: command not found\n", cmd)
