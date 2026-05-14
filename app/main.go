f087b54a31228182f2a8a21b68b457c649c109a5
Omar Adel <oadel1814@gmail.com>
2026-05-15 02:17:32 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
-func prompt() {
+func prompt() string {
 	fmt.Print("$ ")
 	var cmd string
-
-	// blocking the code until the user inputs a line of text and presses enter
-	// read a line of input and store it in the address of the "cmd" variable
 	fmt.Scanln(&cmd)
-	fmt.Printf("%s: command not found\n", cmd)
+	return cmd
 }
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	for {
-		prompt()
+		var cmd string
+		cmd = strings.ToLower(prompt())
+		if cmd == "exit" {
+			break
+		}
+
+		fmt.Printf("%s: command not found\n", cmd)
 	}
 }
